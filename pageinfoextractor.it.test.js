5f49204bc9842e3df9b9d05587887fb6eec69068
Seshagiri Telkapalli Venkata <tvseshagiri@gmail.com>
2023-06-07 19:19:49 +0530

    Added siteUrl part of the response

--- pageinfoextractor.it.test.js
+++ pageinfoextractor.it.test.js
@@ -1,56 +1,56 @@
 const supertest = require('supertest');
 
-const {getTestServer} = require('@google-cloud/functions-framework/testing');
+const { getTestServer } = require('@google-cloud/functions-framework/testing');
 
 require('./index.js')
 
-describe('Intergration Testing::getPageInfo', ()=> {
+describe('Intergration Testing::getPageInfo', () => {
 
     it('getPageInfo with valid url get page details', async () => {
         const server = getTestServer('pageInfoExtractor');
         await supertest(server)
-        .post("/")
-        .send({siteUrl: "https://www.google.com/"})
-        .set('Content-Type', 'application/json')
-        .expect(200)
-        .expect({"title":"Google","desc":"","keywords":""});
+            .post("/")
+            .send({ siteUrl: "https://www.google.com/" })
+            .set('Content-Type', 'application/json')
+            .expect(200)
+            .expect({ "title": "Google", "desc": "", "keywords": "", siteUrl: 'https://www.google.com/' });
     })
 
     it('getPageInfo with invalid Page gets 404 error', async () => {
         const server = getTestServer('pageInfoExtractor');
         await supertest(server)
-        .post("/")
-        .send({siteUrl: "https://www.google.com/dfdfdf.html"})
-        .set('Content-Type', 'application/json')
-        .expect(404)
-        .expect("Request failed with status code 404");
+            .post("/")
+            .send({ siteUrl: "https://www.google.com/dfdfdf.html" })
+            .set('Content-Type', 'application/json')
+            .expect(404)
+            .expect("Request failed with status code 404");
     })
 
     it('getPageInfo with invalid URL gets 400 error', async () => {
         const server = getTestServer('pageInfoExtractor');
         await supertest(server)
-        .post("/")
-        .send({siteUrl: "https://www.nositexists.com/dfdfdf.html"})
-        .set('Content-Type', 'application/json')
-        .expect(400)
-        .expect("getaddrinfo ENOTFOUND www.nositexists.com");
+            .post("/")
+            .send({ siteUrl: "https://www.nositexists.com/dfdfdf.html" })
+            .set('Content-Type', 'application/json')
+            .expect(400)
+            .expect("getaddrinfo ENOTFOUND www.nositexists.com");
     })
 
     it('getPageInfo without URL gets 400 error', async () => {
         const server = getTestServer('pageInfoExtractor');
         await supertest(server)
-        .post("/")
-        .set('Content-Type', 'application/json')
-        .expect(400)
-        .expect("Bad Request with Empty URL");
+            .post("/")
+            .set('Content-Type', 'application/json')
+            .expect(400)
+            .expect("Bad Request with Empty URL");
     })
 
     it('getPageInfo with invalid method gets 405 error', async () => {
         const server = getTestServer('pageInfoExtractor');
         await supertest(server)
-        .get("/")
-        .expect(405)
-        .expect("Method not supported");
+            .get("/")
+            .expect(405)
+            .expect("Method not supported");
     })
 
 })
--- pageinfoextractor.js
+++ pageinfoextractor.js
@@ -1,41 +1,41 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
- function getPageInfo(strUrl) {
+function getPageInfo(strUrl) {
 
     if (!strUrl || strUrl === 'undefined') {
         return {
-                status: 400, 
-                message: `Invalid url '${strUrl}'`
+            status: 400,
+            message: `Invalid url '${strUrl}'`
         }
     }
 
-     pageInfo =  axios.get(strUrl).then(resp => {
-        
+    pageInfo = axios.get(strUrl).then(resp => {
+
         const $ = cheerio.load(resp.data);
         title = $('title').text()
         desc = $("meta[name='description']").attr('content') || $("meta[name='Description']").attr('content') || ''
         keywords = $("meta[name='keywords']").attr('content') || ''
-        return  {
-            title,desc,keywords
+        return {
+            title, desc, keywords, siteUrl: strUrl
         }
     }).catch(err => {
-        console.log(` Error %s`,err)
+        console.log(` Error %s`, err)
 
-        errResp = {type: 'Error'}
+        errResp = { type: 'Error' }
 
         if (err.response) {
             errResp.status = err.response.status;
             errResp.message = err.message;
         } else {
             if (err.message.includes('ENOTFOUND'))
-            errResp.message = err.message;
+                errResp.message = err.message;
             errResp.status = 400
         }
         return errResp
     });
-    
-    return  pageInfo;
+
+    return pageInfo;
 }
 
 module.exports = getPageInfo
